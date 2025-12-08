d414e5cbc93d0f7b42e27c316364fe54c5e05d43
Hiroaki@R-Koubou <dev.r.koubou@gmail.com>
2025-12-09 00:16:25 +0900

    pubspec更新

--- lib/builder.dart
+++ lib/builder.dart
@@ -3,5 +3,6 @@ import 'package:source_gen/source_gen.dart';
 
 import 'src/value_object_generator.dart';
 
+/// Builder for generating value object code
 Builder valueObjectGenerator(BuilderOptions options) =>
     SharedPartBuilder(const [ValueObjectGenerator()], 'simple_value_object');
